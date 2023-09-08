e1a15343f3bcfabe10feb4f3a18746b684d1c85c
sycho <csyull2287@gmail.com>
2023-09-08 20:57:12 +0900

    Revert "fix index.html"
    This reverts commit 96a09b41db7d353b515f74cac3a3cc3c51380915.

--- App.tsx
+++ App.tsx
@@ -1,7 +1,25 @@
+// React CDN을 사용하는 경우, React 라이브러리는 전역 변수인 React에 할당
+const { useState, useEffect } = React;
+
 const App = () => {
+    const [count, setCount] = useState(0);
+    const [currentTime, setCurrentTime] = useState(new Date());
+
+    useEffect(() => {
+        document.title = `Count: ${count}`;
+    }, [count]);
+
+    useEffect(() => {
+        setInterval(() => {
+            setCurrentTime(new Date());
+        }, 1000);
+    }, []);
+
     return (
         <>
             <p>Hello World React</p>
+            <p>Current Time: {currentTime.toLocaleTimeString()}</p>
+            <button onClick={() => setCount(count + 1)}>Increment</button>
         </>
     );
 }
